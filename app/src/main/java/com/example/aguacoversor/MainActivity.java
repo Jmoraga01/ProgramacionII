a0be45eb008830385c87eb889a2a65a53b75606a
Jmoraga01 <158612511+Jmoraga01@users.noreply.github.com>
2024-02-23 22:13:49 -0300

    Parcial_I

---
+++ app/src/main/java/com/example/aguacoversor/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.aguacoversor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn, btnCuota;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btnIr);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent siga = new Intent(MainActivity.this, aguaConversor.class);
+                startActivity(siga);
+
+            }
+        });
+
+        btnCuota = findViewById(R.id.btnCuota);
+        btnCuota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sigaCuota = new Intent(MainActivity.this, cuotaAgua.class);
+                startActivity(sigaCuota);
+            }
+        });
+    }
+}
