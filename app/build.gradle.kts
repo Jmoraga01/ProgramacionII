078fc9c1a9d506b8bca6affb5afe01534b7ac94b
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-05-15 19:28:01 -0600

    creacio de la app y la rama chatAmigos, y introduccion a firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     implementation("com.google.firebase:firebase-storage-ktx")
     implementation("com.google.firebase:firebase-database-ktx")
     implementation("implementation 'com.firebaseui:firebase-ui-storage:8.0.2'")
+    implementation("com.firebaseui:firebase-ui-storage:8.0.2")
 }
