c6aeac4827a790f593fac665a9b52c592c7ed770
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-05-24 19:26:26 -0600

    creacio de nuestra App y de la rama PROYECTO_FIN4L

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity3.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity3.java
@@ -22,11 +22,11 @@ public class MainActivity3 extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         personList = new ArrayList<>();
-        personList.add(new Person("Maurico Alfredo Carranza", "Pharmacist with 10 years of experience.", R.drawable.img_3));
-        personList.add(new Person("Wesly Ariel Umanzor", "Expert in pharmacology and patient care.", R.drawable.img_8));
-        personList.add(new Person("Keyla Canales", "Specialist in medication therapy management.", R.drawable.img_5));
-        personList.add(new Person("Erick Josue Chavez", "Experienced in community pharmacy.", R.drawable.img_6));
-        personList.add(new Person("Daniel Oswaldo Ramirez", "Focused on patient safety and wellness.", R.drawable.img_7));
+        personList.add(new Person("Maurico Alfredo Carranza", "Experto en la programacion, y el ambito tecnologico.", R.drawable.img_3));
+        personList.add(new Person("Wesly Ariel Umanzor", "Experto en la programacion, y el ambito tecnologico.", R.drawable.img_8));
+        personList.add(new Person("Keyla Canales", "Experta en la programacion, y el ambito tecnologico.", R.drawable.img_5));
+        personList.add(new Person("Erick Josue Chavez", "Experto en la programacion, y el ambito tecnologico.", R.drawable.img_6));
+        personList.add(new Person("Daniel Oswaldo Ramirez", "Experto en la programacion, y el ambito tecnologico.", R.drawable.img_7));
 
         personAdapter = new PersonAdapter(this, personList);
         recyclerView.setAdapter(personAdapter);
